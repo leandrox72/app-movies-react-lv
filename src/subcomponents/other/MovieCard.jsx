eb7d35252407c34152788aa69ddbc50ce1dd5664
leandrox72 <leandronvc123@gmail.com>
2024-02-17 20:18:50 -0300

    Muchos Cambios

--- src/subcomponents/other/MovieCard.jsx
+++ src/subcomponents/other/MovieCard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './MovieCard.css'
 
 const MovieCard = ({ img, title, desc }) => {
+
   return (
     <div className='app__card'>
       <img src={img} className='app__card-img'/>
       <div className='app__card-info'>
         <h3>{title}</h3>
-        <p>{desc}</p>
+        <p>{desc.substring(0, 200) + "..."}</p>
       </div>
     </div>
   )
