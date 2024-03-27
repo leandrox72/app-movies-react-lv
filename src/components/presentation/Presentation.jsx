311f78d24b35974190f9faa2b34d0aa0f1b21aa8
Leandro Vaca <leandronvc123@gmail.com>
2024-03-27 08:30:18 -0300

    Cosas...

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -5,16 +5,18 @@ import { useFetchMovies, useFetchMovie } from '../../hooks';
 
 const Presentation = () => {
 
+    // Api cosas...
     const urlBase = import.meta.env.VITE_API_URL;
     const urlBase2 = import.meta.env.VITE_API_URL2;
     const API_KEY = import.meta.env.VITE_API_KEY;
 
+    // Peliculas
     const random = useFetchMovie(`${urlBase}?api_key=${API_KEY}`, true);
     const movies = useFetchMovies(`${urlBase}?api_key=${API_KEY}`);
     const tops = useFetchMovies(`${urlBase2}/top_rated?api_key=${API_KEY}`);
     const upcoming = useFetchMovies(`${urlBase2}/upcoming?api_key=${API_KEY}`);
 
-    // A little bit of non optimal code...
+    // Solucion definitivamente no optima...
     const [ movie, setMovie ] = useState('');
     
     useEffect(() => {
@@ -34,7 +36,7 @@ const Presentation = () => {
                 popularMovies={movies}
                 topMovies={tops}
                 passMovie={passMovie}
-                upcomings={upcoming.slice(0,6)}
+                upcomings={upcoming?.slice(0,6)}
             />
         </>
     )
